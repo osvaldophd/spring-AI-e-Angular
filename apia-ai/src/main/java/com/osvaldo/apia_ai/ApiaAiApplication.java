3c6fe2d1e040aef6e9c4a333252aa4afed504751
osvaldo <phdosvaldoavelino@gmail.com>
2025-06-22 23:00:43 +0100

    criação da api

---
+++ apia-ai/src/main/java/com/osvaldo/apia_ai/ApiaAiApplication.java
@@ -0,0 +1,13 @@
+package com.osvaldo.apia_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiaAiApplication.class, args);
+	}
+
+}
---
+++ apia-ai/src/test/java/com/osvaldo/apia_ai/ApiaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osvaldo.apia_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
